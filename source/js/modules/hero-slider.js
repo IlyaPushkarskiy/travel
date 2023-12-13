ff464b02449f94d10c4cb06a1d1191612f3876fb
IlyaPushkarskiy <p_ilya_007@mail.ru>
2023-12-13 13:08:15 +0300

    день 3 часа 3-4

--- source/js/modules/hero-slider.js
+++ source/js/modules/hero-slider.js
@@ -1,17 +1,24 @@
 const initHeroSlider = () => {
   const heroSlider = document.querySelector('[data-swiper="hero"]');
-  const heroPagination = heroSlider.querySelector('[data-swiper="pagination"]');
+  const heroPagination = document.querySelector('[data-swiper="pagination"]');
 
   let mySwiper = new window.Swiper(heroSlider, {
     loop: true,
-    paganation: {
+    pagination: {
       el: heroPagination,
-      // type: 'bullets',
+      type: 'bullets',
       clickable: true,
     },
     centeredSlides: true,
     slidesPerView: 1,
     spaceBetween: 0,
+
+    breakpoint: {
+      1200: {
+        swipeHandler: false,
+        allowTouchMove: true,
+      },
+    },
   });
   return mySwiper;
 };
